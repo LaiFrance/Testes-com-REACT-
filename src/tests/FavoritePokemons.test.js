1a4000a430fbbfb772c5d969d3a47808cc9610e4
laise <laise94@hotmail.com>
2022-03-27 07:44:22 -0300

    req 3 add cards de pokemons favoritados e msg notfound

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -5,12 +5,15 @@ import pokemons from '../data';
 import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
+  // verifa mensagem caso nao tenha poke favorito
   test('verifica na tela a mensagem :no favorite pokemon found.', () => {
     renderWithRouter(<FavoritePokemons />);
     const msg = screen.getByText(/no favorite pokemon found/i);
     expect(msg).toBeInTheDocument();
   });
 
+  // exiba os cards
+  // informaçoes no data
   test('se é exibido todos os cards de pokémons favoritados.', () => {
     renderWithRouter(
       <FavoritePokemons pokemons={ pokemons } />,
