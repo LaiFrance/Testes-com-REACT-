f8ee350f5299f41bc724c4ff8e7ff16b265f0153
laise <laise94@hotmail.com>
2022-03-28 08:54:54 -0300

    req 6 adiciona card com detalhes e poke favorito

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+// import pokemon from '../data';
+
+describe('Testando o componente Pokemon ', () => {
+  test('Teste se é renderizado um card com as informações de um pokémon.', () => {
+    renderWithRouter(<App />);
+    // CARD COM INFORMAÇÕES DO POKEMON PIKACHU
+    const namePokemon = screen.getByTestId('pokemon-name');
+    const tipoPokemon = screen.getByTestId('pokemon-type');
+    const pesoPokemon = screen.getByTestId('pokemon-weight');
+    const imagem = screen.getByRole('img', { name: /pikachu sprite/i });
+    const linkimage = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+    const linknavega = screen.getByRole('link', { name: /more details/i });
+    // const linkid = '/pokemons/25';
+    expect(namePokemon).toBeInTheDocument('Pikachu');
+    expect(tipoPokemon).toHaveTextContent('Electric');
+    expect(pesoPokemon).toBeInTheDocument('Average weight: 6.0 kg');
+    expect(imagem).toHaveAttribute('src', linkimage);
+    expect(linknavega).toHaveAttribute('href', '/pokemons/25');
+    userEvent.click(linknavega);
+    screen.logTestingPlaygroundURL();
+    // POKEMON FAVORITADO E STAR ICON
+    const pokemonFavoritado = screen.getByText(/pokémon favoritado\?/i);
+    userEvent.click(pokemonFavoritado);
+    const estrela = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(estrela).toBeInTheDocument();
+    expect(estrela).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+/* import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste se é renderizado um card com as informações um pokémon.', () => {
+
+test('verifica se A página tem um texto <name> Details que é o nome do Pokémon;', () => {});
+test('verifica que Não deve existir o link de navegação para os detalhes do Pokémon selecionado', () => {});
+test('verifica se A seção de detalhes deve conter um heading h2 com o texto Summary.', () => {});
+test('verifica se A seção de detalhes deve conter um parágrafo com o resumo do Pokémon específico ', () => {});
+
+describe('Teste se existe na página uma seção com os mapas contendo as localizações do pokémon', () => {});
+
+test('Na seção de detalhes deverá existir um heading h2 com o texto Game Locations of <name>', () => {});
+test('Todas as localizações do Pokémon devem ser mostradas na seção de detalhes', () => {});
+test('Devem ser exibidos, o nome da localização e uma imagem do mapa em cada localização;', () => {});
+test('A imagem da localização deve ter um atributo src com a URL da localização;', () => {});
+test('A imagem da localização deve ter um atributo alt com o texto <name> location, onde <name> é o nome do Pokémon;', () => {}); */
